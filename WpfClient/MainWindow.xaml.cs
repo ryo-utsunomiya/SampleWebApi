da0c581b7ef3c3ceb340e2c322c10ebc908932d5
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-03 16:45:59 +0900

    Add WpfClient

---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void clickGet(object sender, RoutedEventArgs e)
+        {
+            var hc = new HttpClient();
+            var res = await hc.GetAsync("http://localhost:5000/api/People");
+            var str = await res.Content.ReadAsStringAsync();
+            textResult.Text = str;
+        }
+
+        private void clickGetId(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clickPost(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clickPutId(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
